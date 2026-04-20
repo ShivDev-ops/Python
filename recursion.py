5aabcd7fc20003fbf854aa1b936ba06934a7a6fd
ShivDev-ops <shivendra22september@gmail.com>
2026-04-20 23:48:23 +0530

    learnig

---
+++ recursion.py
@@ -0,0 +1,26 @@
+# for num in range(1,7):
+#     re = num(num-1)
+# print(re)
+
+# def fibo(n):
+#     if n==0:
+#         return 0
+#     elif n==1:
+#         return 1
+#     else:
+#         return  fibo(n-1)+fibo(n-2)
+
+# print(fibo(5)) 
+
+# a = input("kill")
+# b = len(a)
+# c = int(a)
+# integer = c//b**(b-10)
+# print(integer)
+
+# a = int(input())
+# num = 0 
+
+import numpy as np 
+
+
