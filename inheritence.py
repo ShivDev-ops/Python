5237157e5abf5615bb839b11d82460e00b45ddd0
ShivDev-ops <shivendra22september@gmail.com>
2026-04-28 13:08:53 +0530

    learning

---
+++ inheritence.py
@@ -0,0 +1,28 @@
+# class Employee:
+#     def __init__(self,name,id):
+#         self.name = name
+#         self.id = id
+
+#     def showinfo(self):
+#         print(f"the name of employee {self.name} and id is {self.id}")
+
+
+# e1 = Employee("Aksahy",12346)
+# e2 = Employee("YouTube",890)
+# e1.showinfo()
+# e2.showinfo()
+
+# class Programmer(Employee):
+#     def showLanguage(self):
+#         print("The default language is python")
+
+# # e1 = Employee("Rohan",90)
+# # e1.showLanguage()                >> Throws error because Employ class have not inherited the objects of child rather than the child have inherited the objects
+# e1 = Programmer("Rohan",90)
+# e1.showLanguage()
+# e1.showinfo()
+
+
+
+
+
